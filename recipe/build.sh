6204fe7397392e614dab4435588afd56692efd44
Ray Donnelly <mingw.android@gmail.com>
2017-08-27 17:37:31 +0100

    Remove MACOSX_DEPLOYMENT_TARGET=10.7

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# default is 10.5 for some reason, which doesn't work
-export MACOSX_DEPLOYMENT_TARGET=10.7
-
-./configure --prefix=$PREFIX
+./configure --prefix=${PREFIX}
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
 make install
