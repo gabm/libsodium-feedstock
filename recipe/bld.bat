9f2c580b4b2116ab05e912597b5a55acdbeb5624
Teake Nutma <t.a.nutma@rug.nl>
2020-08-03 14:40:23 +0200

    Remove vs2008 build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -15,29 +15,12 @@ if %VS_MAJOR% == 9 (
     set "PATH=%CD%;%PATH%"
 )
 
-if "%VS_YEAR%" == "2008" (
-  xcopy /s /y /i %RECIPE_DIR%\vs2008 %SRC_DIR%\builds\msvc\vs%VS_YEAR%
-  copy %LIBRARY_INC%\stdint.h %SRC_DIR%\builds\msvc\
-  copy %LIBRARY_INC%\inttypes.h %SRC_DIR%\builds\msvc\
-  cd /d %SRC_DIR%\builds\msvc\vs%VS_YEAR%\dynamic
-  msbuild libsodium.sln /p:Configuration=Release /p:Platform=%ARCH%
-  if errorlevel 1 exit 1
-  cd /d %SRC_DIR%\builds\msvc\vs%VS_YEAR%\static
-  msbuild libsodium.sln /p:Configuration=Release /p:Platform=%ARCH%
-  if errorlevel 1 exit 1
-  :: Generate version.h
-  cd /d %SRC_DIR%
-  call msvc-scripts\process.bat
-  if errorlevel 1 exit 1
-  set ARTIFACTS_DIR=%SRC_DIR%\bin\%ARCH%\Release\v90
-) else (
-  cd /d %SRC_DIR%\builds\msvc\vs%VS_YEAR%\
-  msbuild libsodium.sln /p:Configuration=DynRelease /p:Platform=%ARCH%
-  if errorlevel 1 exit 1
-  msbuild libsodium.sln /p:Configuration=StaticRelease /p:Platform=%ARCH%
-  if errorlevel 1 exit 1
-  set ARTIFACTS_DIR=%SRC_DIR%\bin\%ARCH%\Release\v140
-)
+cd /d %SRC_DIR%\builds\msvc\vs%VS_YEAR%\
+msbuild libsodium.sln /p:Configuration=DynRelease /p:Platform=%ARCH%
+if errorlevel 1 exit 1
+msbuild libsodium.sln /p:Configuration=StaticRelease /p:Platform=%ARCH%
+if errorlevel 1 exit 1
+set ARTIFACTS_DIR=%SRC_DIR%\bin\%ARCH%\Release\v140
 
 if "%VS_YEAR%" == "2010" (
   set ARTIFACTS_DIR=%SRC_DIR%\bin\%ARCH%\Release\v100
