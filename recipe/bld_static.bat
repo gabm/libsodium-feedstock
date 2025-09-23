75f71a514cbafff340a45f8ecf2481708062ef97
root <root@pc-nico.localdomain>
2025-09-23 06:25:00 +0200

    add split package for static lib

---
+++ recipe/bld_static.bat
@@ -0,0 +1,15 @@
+@echo on
+
+if "%ARCH%" == "32" (
+  set ARCH=Win32
+) else (
+  set ARCH=x64
+)
+
+
+cd /d %SRC_DIR%\builds\msvc\vs%VS_YEAR%\
+msbuild libsodium.sln /p:Configuration=StaticRelease /p:Platform=%ARCH%
+if errorlevel 1 exit 1
+set ARTIFACTS_DIR=%SRC_DIR%\bin\%ARCH%\Release\v142
+
+move /y %ARTIFACTS_DIR%\static\libsodium.lib %LIBRARY_LIB%\libsodium_static.lib
